3a8d53193e0e2deeeee6179d363a35da2bffed27
Juan Pablo <juan.gaitan@uao.edu.co>
2023-02-10 09:11:56 -0500

    creacion crud

---
+++ app/Http/Controllers/OrdenController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrdenController extends Controller
+{
+    public function index(Request $destination){
+        //return view('vista')
+
+    }
+}
